50b6a521203bc8f501f1dd3195bfc9223b16e0b1
Tay <taylor.falk3@gmail.com>
2020-05-23 17:59:21 -0400

    okay now we're warming up

---
+++ webapp.js
@@ -0,0 +1,11 @@
+var http = require("http")
+
+http.createServer(onRequest).listen(8888);
+console.log('Server started');
+
+function onRequest(request, response) {
+	response.writeHead(200);
+	response.write('Hello worm');
+	response.end();
+}
+
